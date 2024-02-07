df8a6c6096f5d090ea9a998cf4f302f778f7cbb1
manaljosid <mani.magnusson@hotmail.com>
2024-02-07 19:21:50 +0000

    Add axi-steam_if.sv

--- rtl/syn/axi-stream/axi-stream_if.sv
+++ rtl/syn/axi-stream/axi-stream_if.sv
@@ -1,23 +1,74 @@
 /**
- * @file axi-lite_if.sv
+ * @file axi-stream_if.sv
  *
  * @author Mani Magnusson
  * @date   2023
  *
- * @brief AXI4-Lite Interface Definition, uses a 32 bit bus
+ * @brief AXI-Stream Interface Definition
  */
 
- /*
-  * TODO:
-  *  - Add this shi
-  */
+`default_nettype none
 
-  `default_nettype none
+interface AXIS_IF # (
+  parameter TDATA_WIDTH   = 32,
+  parameter TID_WIDTH     = ??,
+  parameter TDEST_WIDTH   = ??,
+  parameter TUSER_WIDTH   = ??
+);
+    // Signals
+    var logic                       tvalid;
+    var logic                       tready;
+    var logic [TDATA_WIDTH-1:0]     tdata;
+    var logic [(TDATA_WIDTH/8)-1:0] tstrb;
+    var logic [(TDATA_WIDTH/8)-1:0] tkeep;
+    var logic                       tlast;
+    var logic [TID_WIDTH-1:0]       tid;
+    var logic [TDEST_WIDTH-1:0]     tdest;
+    var logic [TUSER WIDTH-1:0]     tuser;
+    var logic                       twakeup;
 
-  interface AXIS_IF # ();
-  // Signals
+    modport Transmitter (
+        output tvalid,
+        input tready,
 
+        output tdata,
+        output tstrb,
+        output tkeep,
+        output tlast,
+        output tid,
+        output tdest,
+        output tuser,
+        output twakeup
+    );
 
+    modport Receiver (
+        input tvalid,
+        output tready,
 
+        input tdata,
+        input tsrb,
+        input tkeep,
+        input tlast,
+        input tid,
+        input tdest,
+        input tuser,
+        input twakeup
+    );
 
-  `default_nettype wire
+    modport Monitor (
+        input tvalid,
+        input tready,
+
+        input tdata,
+        input tstrb,
+        input tkeep,
+        input tlast,
+        input tid,
+        input tdest,
+        input tuser,
+        input twakeup
+    )
+
+endinterface
+
+`default_nettype wire
