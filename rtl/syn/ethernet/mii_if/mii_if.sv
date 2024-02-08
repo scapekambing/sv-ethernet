58dc6a77f3b0bd5104484f086ba912116e0b5a5c
manaljosid <mam90@hi.is>
2024-02-08 13:45:56 +0000

    Remove optional tx_er signal

--- rtl/syn/ethernet/mii_if/mii_if.sv
+++ rtl/syn/ethernet/mii_if/mii_if.sv
@@ -15,47 +15,51 @@ interface MII_IF # (
     var logic       tx_clk;
     var logic [3:0] txd;
     var logic       tx_en;
-    var logic       tx_er;
 
     var logic       rx_clk;
     var logic [3:0] rxd;
     var logic       rx_dv;
     var logic       rx_er;
+    var logic       crs;
+    var logic       col;
 
     modport MAC (
         input tx_clk,
         output txd,
         output tx_en,
-        output tx_er,
         
         input rx_clk,
         input rxd,
         input rx_dv,
-        input rx_er
+        input rx_er,
+        input crs,
+        input col
     );
 
     modport PHY (
         output tx_clk,
         input txd,
         input tx_en,
-        input tx_er,
         
         output rx_clk,
         output rxd,
         output rx_dv,
-        output rx_er
+        output rx_er,
+        output crs,
+        output col
     );
 
     modport Monitor (
         input tx_clk,
         input txd,
         input rx_en,
-        input tx_er,
         
         input rx_clk,
         input rxd,
         input rx_dv,
-        input rx_er
+        input rx_er,
+        input crs,
+        input col
     );
 
 endinterface
