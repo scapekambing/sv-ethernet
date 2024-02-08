a14eb793aa1ff94520566ab5686f50294208c310
manaljosid <mam90@hi.is>
2024-02-08 13:03:14 +0000

    Create top module for the ethernet stack

---
+++ rtl/syn/ethernet/eth_top.sv
@@ -0,0 +1,20 @@
+/**
+ * @file eth_top.sv
+ *
+ * @author Mani Magnusson
+ * @date   2024
+ *
+ * @brief Top module of Ethernet stack
+ */
+
+ `default_nettype none
+
+ module eth_top # (
+    // Parameters
+ ) (
+    // Signals
+ );
+    // Logic
+ endmodule
+
+ `default_nettype wire
