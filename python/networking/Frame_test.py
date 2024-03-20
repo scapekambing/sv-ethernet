75912a02f937c9e3e156b8472639d82da4a8fe7a
manaljosid <mam90@hi.is>
2024-03-20 12:59:56 +0000

    Initial commit

--- python/networking/Frame_test.py
+++ python/networking/Frame_test.py
@@ -1,1 +1,14 @@
 from scapy.all import *
+import socket
+
+UDP_IP = "192.168.1.128"
+UDP_PORT = 1234
+MESSAGE = b"Hello world!"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+sock.bind((UDP_IP, UDP_PORT))
+
+data, addr = sock.recvfrom(16)
+print("Recieved message: %s" % data)
