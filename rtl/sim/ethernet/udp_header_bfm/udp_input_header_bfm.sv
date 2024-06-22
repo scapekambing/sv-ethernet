f2a009247ef97ab3a35b93447c48896000977886
manaljosid <mani.magnusson@hotmail.com>
2024-06-22 02:30:05 +0000

    Create udp header bfms

---
+++ rtl/sim/ethernet/udp_header_bfm/udp_input_header_bfm.sv
@@ -0,0 +1,15 @@
+/**
+ * @file udp_input_header_bfm.sv
+ * 
+ * @author Mani Magnusson
+ * @date   2024
+ * 
+ * @brief UDP Header (PHY to logic) bus functional model
+ */
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+
+
+`default_nettype wire
