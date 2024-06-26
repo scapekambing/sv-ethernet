6d71192c902b3f394e95ade38d4c49a97aefc017
manaljosid <mani.magnusson@hotmail.com>
2024-06-26 16:24:50 +0000

    Rename udp header bfms

--- rtl/sim/ethernet/udp_header_bfm/udp_rx_header_bfm.sv
+++ rtl/sim/ethernet/udp_header_bfm/udp_rx_header_bfm.sv
@@ -1,10 +1,10 @@
 /**
- * @file udp_input_header_bfm.sv
+ * @file udp_rx_header_bfm.sv
  * 
  * @author Mani Magnusson
  * @date   2024
  * 
- * @brief UDP Header (PHY to logic) bus functional model
+ * @brief UDP RX Header (PHY to logic) bus functional model
  */
 
 `timescale 1ns / 1ps
---
+++ rtl/sim/ethernet/udp_header_bfm/udp_tx_header_bfm.sv
@@ -0,0 +1,27 @@
+/**
+ * @file udp_tx_header_bfm.sv
+ * 
+ * @author Mani Magnusson
+ * @date   2024
+ * 
+ * @brief UDP TX Header (logic to PHY) bus functional model
+ */
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+package udp_tx_header_bfm;
+class UDP_TX_HEADER_SLAVE_BFM # (
+    // Parameters
+);
+    // Do stuff
+endclass
+
+class UDP_TX_HEADER_MASTER_BFM # (
+    // Parameters
+);
+    // Do stuff
+endclass
+endpackage
+
+`default_nettype wire
