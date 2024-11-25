1a9c7f77ea6158ade37d83aeb861fd9f4eb3ae92
scapekambing <e.firmansyah@outlook.com>
2024-11-25 13:56:17 -0800

    ADD: python client-server sockets test

---
+++ python/networking/client.py
@@ -0,0 +1,73 @@
+import socket
+import argparse
+
+class Client:
+  def __init__(self, ip, port, data):
+    self.ip = ip
+    self.port = port
+    self.data = data
+    self.s = None
+  
+  def begin(self):
+    self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.s.settimeout(10)
+    self.s.sendto(self.data, (self.ip, self.port))
+    print(f"Client sent {self.data} to {self.ip}:{self.port}")
+    while True:
+      try: 
+        message, addr = self.s.recvfrom(1024)
+        print(f"Message received: {message} from {addr}")
+        self.on_message(message, addr)
+        break
+        
+      except OSError as e:
+        if isinstance(e, socket.timeout):
+          print("\nSocket timed out. Client is shutting down...")
+          self.s.close()
+          break
+        if isinstance(e, KeyboardInterrupt):
+          print("\nKeyboard interrupt. Client is shutting down...")
+          self.s.close()
+          break
+        else:
+          raise
+    print("Client is shutting down. Closing socket connection...")
+    self.s.close()
+
+  def on_message(self, message, addr):
+    if message == b"ACK":
+      self.on_ack(addr)
+    elif message == b"ECHO":
+      self.on_echo(addr)
+    else:
+      print("message received: ", message)
+
+  def on_ack(self, addr):
+    print("\033[92m ACK \033[0m", "| from: ", addr)
+
+  def on_echo(self, addr):
+    print("ECHO server started. Type 'exit' to exit.")
+    send_message = input("Enter message: ")
+    self.s.sendto(bytes(send_message, "utf-8"), addr)
+    while True:
+      if send_message == "exit":
+        break
+      else:
+        recv_message, addr = self.s.recvfrom(1024)
+        if recv_message:
+          print(f"Message received: {recv_message} from {addr}")
+          send_message = input("Enter message: ")
+          self.s.sendto(bytes(send_message, "utf-8"), addr)
+    
+
+def main(ip, port, data):
+  c = Client(ip, port, data)
+  c.begin()
+  
+if __name__ == '__main__':
+  argparse = argparse.ArgumentParser(description='Socket communication script.')
+  argparse.add_argument('--ip', type=str, default='localhost', help='IP address to connect to')
+  argparse.add_argument('--port', type=int, default=1234, help='Port to connect to')
+  argparse.add_argument('--data', type=str, default="ACK", help='Data to send')
+  args = argparse.parse_args()
+  main(args.ip, args.port, bytes(args.data, "utf-8"))
---
+++ python/networking/server.py
@@ -0,0 +1,70 @@
+import socket
+import argparse
+
+class Server:
+  def __init__(self, ip, port):
+    self.addr = (ip, port)
+    self.ip = ip
+    self.port = port
+    self.s = None
+  
+  def begin(self):
+    self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.s.settimeout(10)
+    self.s.bind((self.ip, self.port))
+    print(f"Server started on {self.ip}:{self.port}")
+    while True:
+      try: 
+        message, addr = self.s.recvfrom(1024)
+        print(f"Message received: {message} from {addr}")
+        self.on_message(message, addr)
+        break
+        
+      except OSError as e:
+        if isinstance(e, socket.timeout):
+          print("\nSocket timed out. Server is shutting down...")
+          self.s.close()
+          break
+        if isinstance(e, KeyboardInterrupt):
+          print("\nKeyboard interrupt. Server is shutting down...")
+          self.s.close()
+          break
+        else:
+          raise
+    print("Server is shutting down. Closing socket connection...")
+    self.s.close()
+
+  def on_message(self, message, addr):
+    if message == b"ACK":
+      self.s.sendto(message, addr)
+      print(f"ACK sent to {addr}")
+    elif message == b"ECHO":
+      self.s.sendto(message, addr)
+      self.on_echo(addr)
+    elif message == b"exit":
+      print("Server is shutting down...")
+    else:
+      print(f"Command not recognized: {message}")
+
+  def on_echo(self, addr):
+    print("Starting ECHO server...")
+    message, addr = self.s.recvfrom(1024)
+    while message != b"exit":
+      message, addr = self.s.recvfrom(1024)
+      print(f"Message received: {message} from {addr}")
+      self.s.sendto(message, addr)
+      print(f"Message sent: {message}")
+    print("ECHO server is shutting down...")
+    
+
+def main(ip, port):
+  s = Server(ip, port)
+  s.begin()
+    
+if __name__ == '__main__':
+  argparse = argparse.ArgumentParser(description='Socket communication script.')
+  argparse.add_argument('--ip', type=str, default='localhost', help='IP address to connect to')
+  argparse.add_argument('--port', type=int, default=1234, help='Port to connect to')
+  args = argparse.parse_args()
+
+  main(args.ip, args.port)
